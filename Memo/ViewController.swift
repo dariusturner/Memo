1c1142d855c29244672bba48547bda950e0e495b
Darius Turner <dariusot731@gmail.com>
2019-07-21 06:23:49 -0400

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Darius Turner on 7/21/19.
+//  Copyright © 2019 Darius Turner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
